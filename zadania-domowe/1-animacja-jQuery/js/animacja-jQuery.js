8e57a76994d99a05cb00b97a7b9eb9b96f1d6c0b
Ewa Rogulska <ewarogulska@wp.pl>
2018-01-24 16:48:22 +0100

    zadanie domowe 1-animacja-jQuery

--- zadania-domowe/1-animacja-jQuery/js/animacja-jQuery.js
+++ zadania-domowe/1-animacja-jQuery/js/animacja-jQuery.js
@@ -1,23 +1,31 @@
 'use strict';
 
-$ (function () {
- 
+$(function () {
+
     var przycisk = $("button");
-//    console.log(przycisk);
-    
+    //    console.log(przycisk);
+
     var kwadrat = $("#square");
-//    console.log(kwadrat);
-      
-    $("button").click(function() {
-        
-    $("#square").animate({left: "100px", "height": "100px", "width" : "100px"}, 3000).delay(5000).queue (function (next) {
-    
-    $("#square").css({"background": "blue"}).append("Animacja zakończona!")
-        ;
-        
+    //    console.log(kwadrat);
+
+    $("button").click(function () {
+
+        $("#square").animate({
+            "left": "100px",
+            "height": "100px",
+            "width": "100px"
+        }, 3000).queue(function (next) {
+
+            $("#square").css({
+                "background": "blue",
+                "transition": "5s"
+            }).append("Animacja zakończona!");
+
+
+        })
+
     });
 
 });
-    });
 
     
