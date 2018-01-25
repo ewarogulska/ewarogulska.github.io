fddff0ddf2ea83bb53f4b48c51882d862e0348cd
Ewa Rogulska <ewarogulska@wp.pl>
2018-01-25 18:12:50 +0100

    jQuery-suma wypłat
    Zaprogramuj przycisk #count-sum aby po kliknięciu pobrał wypłaty wszystkich pracowników i je zsumował.
    Uzyskany wynik wyświetl w elemencie o ID #sum.
    
    Zadanie zrób z wykorzystaniem biblioteki jQuery.
    
    Podpowiedzi:
    - oprogramuj zdarzenie "click" przycisku #count-sum
    - pobierz wszystkie wypłaty i za pomocą pętli je zsumuj
    - pobierz element o ID #sum i podmień mu treść na obliczoną sumę

---
+++ zadania-domowe/jQuery-suma-wyplat/js/jQuery-suma-wyplat.js
@@ -0,0 +1,22 @@
+'use strict';
+
+$(document).ready(function () {
+
+    $("#count-sum").click(function () {
+
+        //    $(".salary").each(function() {
+        //         console.log($(this).text());
+        //     });
+
+        $(".salary").each(function () {
+            var suma = 0;
+
+            $(".salary").each(function () {
+                suma += +$(this).text() || 0;
+            });
+
+            $("#sum").text(suma);
+        });
+
+    });
+});
